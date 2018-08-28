55b2468429b387380bc59673587d9759701c36dd
kn1cht <ito.turukasiwa@gmail.com>
2018-08-29 01:21:49 +0900

    Add script

---
+++ HiyokoAttack/Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 8f;
+	public float moveableRange = 5.5f;
+
+	void Update() {
+		transform.Translate(Input.GetAxisRaw ("Horizontal") * speed * Time.deltaTime, 0, 0);
+		transform.position = new Vector2(
+			Mathf.Clamp(transform.position.x, -moveableRange, moveableRange),
+			transform.position.y
+		);
+	}
+}
