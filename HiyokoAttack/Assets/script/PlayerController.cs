17b97e0e9a07e06afce01ab59109823c4b040104
kn1cht <ito.turukasiwa@gmail.com>
2018-09-01 15:32:48 +0900

    Shooter

--- HiyokoAttack/Assets/script/PlayerController.cs
+++ HiyokoAttack/Assets/script/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed = 8f;
 	public float moveableRange = 5.5f;
+	public float power = 1000f;
+	public GameObject cannonBall;
+	public Transform spawnPoint;
 
 	void Update() {
 		transform.Translate(Input.GetAxisRaw ("Horizontal") * speed * Time.deltaTime, 0, 0);
@@ -13,5 +16,12 @@ public class PlayerController : MonoBehaviour {
 			Mathf.Clamp(transform.position.x, -moveableRange, moveableRange),
 			transform.position.y
 		);
+
+		if(Input.GetKeyDown(KeyCode.Space)) { Shoot(); }
+	}
+
+	void Shoot() {
+		GameObject newBullet = Instantiate(cannonBall, spawnPoint.position, Quaternion.identity) as GameObject;
+		newBullet.GetComponent<Rigidbody2D>().AddForce(Vector3.up * power);
 	}
 }
