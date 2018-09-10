7d66f092f0b3ded2b4f5f7abe9636eae955a76bf
kn1cht <ito.turukasiwa@gmail.com>
2018-09-11 03:51:10 +0900

    add goal checking

---
+++ StageRun/Assets/GoalCheck.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalCheck : MonoBehaviour {
+	public static bool goal;
+
+	void Start() { goal = false; }
+
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "Player") { goal = true; }
+	}
+}
