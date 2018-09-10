c1ca2fed0529ae53dfbda0f43cc658609d5c913f
kn1cht <ito.turukasiwa@gmail.com>
2018-09-11 03:29:11 +0900

    reset function

---
+++ StageRun/Assets/OutReset.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OutReset : MonoBehaviour {
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "Player") {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
