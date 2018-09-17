96e9353dcf053f38563dc52bc25d93e081b18299
kn1cht <ito.turukasiwa@gmail.com>
2018-09-17 20:19:26 +0900

    Add Game Result

---
+++ StageRun/Assets/GameResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameResult : MonoBehaviour {
+	private int highScore;
+	public Text resultTime;
+	public Text bestTime;
+	public GameObject resultUI;
+
+	// Use this for initialization
+	void Start () {
+		if(PlayerPrefs.HasKey("HighScore")) {
+			highScore = PlayerPrefs.GetInt("HighScore");
+		}
+		else {
+			highScore = 999;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(GoalCheck.goal) {
+			resultUI.SetActive(true);
+			int result = Mathf.FloorToInt(Timer.time);
+			resultTime.text = "Time: " + result;
+			bestTime.text = "BestTime: " + Math.Min(highScore, result);
+			if(highScore > result) {
+				PlayerPrefs.SetInt("HighScore", result);
+			}
+		}
+	}
+
+	public void OnRetry() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
